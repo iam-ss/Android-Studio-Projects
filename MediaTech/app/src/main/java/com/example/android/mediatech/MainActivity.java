a5ac0d339eaef7de3bf6dbf02c390310cd44f350
Shubham Singh <singhams.shubham571@gmail.com>
2017-01-22 00:56:04 +0530

    add backstree.mp3 song to check the capabilitits of MediaPlayer class.

--- MediaTech/app/src/main/java/com/example/android/mediatech/MainActivity.java
+++ MediaTech/app/src/main/java/com/example/android/mediatech/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.android.mediatech;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MediaPlayer mediaPlayer=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button play = (Button) findViewById(R.id.play_btn);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer == null) 
+                    mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.backstreet);
+                mediaPlayer.start();
+            }
+        });
+
+        Button pause = (Button) findViewById(R.id.pause_btn);
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer.isPlaying())
+                    mediaPlayer.pause();
+            }
+        });
+
+
     }
 }
