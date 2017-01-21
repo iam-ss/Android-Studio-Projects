9970a9cbd37247ff081528738bb76d506c468f68
Shubham Singh <singhams.shubham571@gmail.com>
2017-01-22 02:28:03 +0530

    Add volume up and volume down buttons and add their respective functionality in their java files

--- MediaTech/app/src/main/java/com/example/android/mediatech/MainActivity.java
+++ MediaTech/app/src/main/java/com/example/android/mediatech/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.mediatech;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mediaPlayer == null) 
+                if(mediaPlayer == null)
                     mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.backstreet);
                 mediaPlayer.start();
             }
@@ -32,7 +33,26 @@ public class MainActivity extends AppCompatActivity {
                     mediaPlayer.pause();
             }
         });
+    }
 
+    public void decrease_volume(View view){
+        AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+        float volume_level= am.getStreamVolume(AudioManager.STREAM_MUSIC);
+        float maxVolume = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        if(volume_level <= 0.0) return ;
+        float newVolume  = Math.max(0.0f, volume_level - 0.05f*maxVolume);
+
+        am.setStreamVolume(AudioManager.STREAM_MUSIC,(int)Math.floor(newVolume),AudioManager.FLAG_SHOW_UI);
+    }
+
+    public void increase_volume(View view){
+        AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+        float volume_level= am.getStreamVolume(AudioManager.STREAM_MUSIC);
+        float maxVolume = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        if(volume_level >= maxVolume) return ;
+        float newVolume  = Math.min(maxVolume,volume_level + 0.05f*maxVolume);
+
+        am.setStreamVolume(AudioManager.STREAM_MUSIC,(int)Math.ceil(newVolume),AudioManager.FLAG_SHOW_UI);
 
     }
 }
