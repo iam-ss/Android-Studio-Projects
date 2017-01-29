cb0277c6e59890d80f13c738c1b60cbd6a094ada
singham <singhams.shubham571@gmail.com>
2017-01-30 01:03:02 +0530

    Update androidManifest.xml file . Add parent activity attribute in every activity tag to support the up botton within every activity.

--- PracticeApp/app/src/main/java/com/example/android/practiceapp/MainActivity.java
+++ PracticeApp/app/src/main/java/com/example/android/practiceapp/MainActivity.java
@@ -3,38 +3,47 @@ package com.example.android.practiceapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RadioGroup radioSexGroup;
-    private RadioButton radioSexButton;
-    private Button btnDisplay;
-
-    @Override
+    int total=0;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addListenerOnButton();
+        TextView tv=(TextView) findViewById(R.id.textView);
+        tv.setText("0");
     }
 
-    public void addListenerOnButton(){
-
-        radioSexGroup = (RadioGroup) findViewById(R.id.radioSex);
-        btnDisplay = (Button) findViewById(R.id.btnDisplay);
-
-        btnDisplay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int selecteId = radioSexGroup.getCheckedRadioButtonId();
-                radioSexButton = (RadioButton) findViewById(selecteId);
-
-                Toast.makeText(MainActivity.this,radioSexButton.getText(),Toast.LENGTH_SHORT).show();
-
-            }
-        });
+    public void Change (int a)
+    {
+        TextView tv=(TextView) findViewById(R.id.textView);
+        tv.setText(String.valueOf(a));
     }
+
+    public void Plus(View v)
+    {
+        if(total==10)
+        {
+            Toast.makeText(this,"More Orders not Possible",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            total = total + 1;
+            Change(total);
+        }
+    }
+
+
+    public void Minus(View v)
+    {
+        if(total==0)
+            Toast.makeText(this,"Not Possible",Toast.LENGTH_SHORT).show();
+
+        else {
+            total = total - 1;
+            Change(total);
+        }
+    }
+
 }
