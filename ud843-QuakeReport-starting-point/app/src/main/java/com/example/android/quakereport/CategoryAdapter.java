875773d6cd80f644a36ccadff76960767f949bc5
singham <singhams.shubham571@gmail.com>
2017-02-14 02:26:32 +0530

    Make custom Category Adapter and a new Information Class to hold earthquake information.

---
+++ ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/CategoryAdapter.java
@@ -0,0 +1,47 @@
+package com.example.android.quakereport;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by singham on 14/2/17.
+ */
+
+public class CategoryAdapter extends ArrayAdapter {
+
+    Context mContext;
+
+    public CategoryAdapter(Context context, ArrayList<Information> earthquake_info) {
+        super(context, 0, earthquake_info);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if(convertView==null){
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item,parent,false
+            );
+        }
+
+        Information info =(Information) getItem(position);
+        TextView tv= (TextView) listItemView.findViewById(R.id.magView);
+        tv.setText(String.valueOf(info.mMag));
+
+        tv = (TextView) listItemView.findViewById(R.id.cityView);
+        tv.setText(info.mCity);
+
+        tv = (TextView) listItemView.findViewById(R.id.dateView);
+        tv.setText(String.valueOf(info.mDate));
+
+        return listItemView;
+    }
+}
---
+++ ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/Information.java
@@ -0,0 +1,18 @@
+package com.example.android.quakereport;
+
+/**
+ * Created by singham on 14/2/17.
+ */
+
+public class Information {
+    double mMag;
+    String mCity;
+    String mDate;
+
+    public Information(double mag,String city,String date){
+        mMag = mag;
+        mCity = city;
+        mDate = date;
+    }
+
+}
