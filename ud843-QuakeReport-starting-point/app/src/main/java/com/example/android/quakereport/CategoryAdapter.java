be85c41f0303fa22a89a36f86806f97dc8972b45
singham <singhams.shubham571@gmail.com>
2017-02-16 18:15:36 +0530

    Make own shapes using <shape > tag in a layout file in drawable directory and set it as a background of Magnitude  text View. Also split the location in two separate strings and change the time to 12 Hr format.

--- ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/CategoryAdapter.java
+++ ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/CategoryAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.quakereport;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 /**
@@ -22,6 +24,19 @@ public class CategoryAdapter extends ArrayAdapter {
         super(context, 0, earthquake_info);
     }
 
+    private int getMagnitudeColor(double mag){
+        if(mag<2.0)return R.color.magnitude1;
+        else if(mag<3.0)return R.color.magnitude2;
+        else if(mag<4.0)return R.color.magnitude3;
+        else if(mag<5.0)return R.color.magnitude4;
+        else if(mag<6.0)return R.color.magnitude5;
+        else if(mag<7.0)return R.color.magnitude6;
+        else if(mag<8.0)return R.color.magnitude7;
+        else if(mag<9.0)return R.color.magnitude8;
+        else if(mag<10.0)return R.color.magnitude9;
+        else return R.color.magnitude10plus;
+   }
+
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -32,15 +47,45 @@ public class CategoryAdapter extends ArrayAdapter {
             );
         }
 
+
         Earthquake info =(Earthquake) getItem(position);
         TextView tv= (TextView) listItemView.findViewById(R.id.magView);
-        tv.setText(String.valueOf(info.getmMag()));
 
-        tv = (TextView) listItemView.findViewById(R.id.cityView);
-        tv.setText(info.getmCity());
+
+        double mag = info.getmMag();
+        DecimalFormat df = new DecimalFormat("0.0");
+        tv.setText(df.format(mag));
+
+        GradientDrawable magnitudeCircle = (GradientDrawable) tv.getBackground();
+        int magnitudeColor = getMagnitudeColor(mag);
+        magnitudeCircle.setColor(listItemView.getResources().getColor(magnitudeColor));
+
+        if(info.getmCity().contains("of")){
+            String[] completeLocation = info.getmCity().split("(?<=of)");
+
+            tv = (TextView) listItemView.findViewById(R.id.nearBy);
+            tv.setText(completeLocation[0]);
+
+            tv = (TextView) listItemView.findViewById(R.id.cityView);
+            tv.setText(completeLocation[1]);
+        }
+        else{
+            tv = (TextView) listItemView.findViewById(R.id.nearBy);
+            tv.setText("Near The");
+
+            tv = (TextView) listItemView.findViewById(R.id.cityView);
+            tv.setText(info.getmCity());
+
+        }
+
+
+        String[] timeAndDate = String.valueOf(info.getmDate()).split("\n");
 
         tv = (TextView) listItemView.findViewById(R.id.dateView);
-        tv.setText(String.valueOf(info.getmDate()));
+        tv.setText(timeAndDate[0]);
+
+        tv = (TextView) listItemView.findViewById(R.id.time);
+        tv.setText(timeAndDate[1]);
 
         return listItemView;
     }
